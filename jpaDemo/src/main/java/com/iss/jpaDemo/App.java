1ff3059079fb744d35adec0ac5d2078760720088
bai owen <owenbai101@gmail.com>
2018-06-11 15:46:32 +0800

    try

---
+++ jpaDemo/src/main/java/com/iss/jpaDemo/App.java
@@ -0,0 +1,15 @@
+package com.iss.jpaDemo;
+
+import javax.persistence.EntityManager;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	
+    }
+}
---
+++ jpaDemo/src/main/java/com/iss/jpaDemo/Student.java
@@ -0,0 +1,38 @@
+package com.iss.jpaDemo;
+
+public class Student {
+	private String name;
+	private String nric;
+	private String nickname;
+	private int mark;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getNric() {
+		return nric;
+	}
+	public void setNric(String nric) {
+		this.nric = nric;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public int getMark() {
+		return mark;
+	}
+	public void setMark(int mark) {
+		this.mark = mark;
+	}
+	@Override
+	public String toString() {
+		return "Student [name=" + name + ", nric=" + nric + ", nickname=" + nickname + ", mark=" + mark + "]";
+	}
+	
+
+}
